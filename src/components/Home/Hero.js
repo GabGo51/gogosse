474d8e814d531946fa767c3f6ce675e1cd369e68
GabGo51 <snow.gabo51@gmail.com>
2023-11-26 13:10:34 -0500

    movement smooth

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -1,17 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import styled, { keyframes } from "styled-components";
 import projects from "../../data/projectData";
-import { useContext } from "react";
 import { MouseContext } from "../../context/mouseContext";
 import { useNavigate } from "react-router-dom";
+import {motion} from "framer-motion"
 
 const Hero = () => {
-  const {cursorChangeHandler } = useContext(MouseContext);
+  const [mouseX, setMouseX] = useState(0);
+  const { cursorChangeHandler } = useContext(MouseContext);
 
   const [activeProject, setActiveProject] = useState(projects[0]);
 
+  const spring = {
+    type: "spring",
+    damping: 9,
+    stiffness: 50
+  }
+
   const handleHover = (projectTitle) => {
-    cursorChangeHandler('hover')
+    cursorChangeHandler("hover");
     const selectedProject = projects.find(
       (project) => project.title === projectTitle
     );
@@ -26,64 +33,111 @@ const Hero = () => {
     navigate(page);
   };
 
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      const adjustedMouseX = e.clientX - 340;
+      const minX = 0;
+      const maxX = window.innerWidth - 670;
+
+      if (window.innerWidth > 1100) {
+        
+        setMouseX(Math.max(minX, Math.min(adjustedMouseX, maxX)));
+      }else{
+        setMouseX(0)
+      }
+    };
+    document.addEventListener("mousemove", handleMouseMove);
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
   
+
   return (
     <Container>
-      <div className="top">
-        
-        <ImgBox>
-          <div>
+      <ImgBox animate={{x: mouseX}} transition={spring}>
+        <div>
+          {projects.map((project) => (
+            <img
+              key={project.title}
+              alt="project"
+              src={
+                project.title === activeProject.title
+                  ? project.img
+                  : project.img
+              }
+              className={
+                project.title === activeProject.title ? "" : "fade-out"
+              }
+              onMouseEnter={() => cursorChangeHandler("hover")}
+              onMouseLeave={() => cursorChangeHandler("")}
+              onClick={() => handleNavigate(`/${project.title}`)}
+            />
+          ))}
+          
+        </div>
+        <div className="project-text">
             <p>{activeProject.title}</p>
             <p>{activeProject.type}</p>
-          </div>
-          <div className="cadre">
-            {projects.map((project) => (
-              <img
-                key={project.title}
-                alt="project"
-                src={
-                  project.title === activeProject.title
-                    ? project.img
-                    : project.img
-                }
-                className={
-                  project.title === activeProject.title ? "" : "fade-out"
-                }
-                onMouseEnter={() => cursorChangeHandler("hover")}
-                onMouseLeave={() => cursorChangeHandler("")}
-                onClick={() => handleNavigate(`/${project.title}`)}
-              />
-            ))}
-          </div>
-        </ImgBox>
-      </div>
+        </div>
+      </ImgBox>
+
       <LettersBox>
         <div className="box" onMouseLeave={() => cursorChangeHandler("")}>
-          <div className="g1" onMouseEnter={() => handleHover(projects[0].title)} onClick={() => handleNavigate(`/${projects[0].title}`)}>
+          <div
+            className="g1"
+            onMouseEnter={() => handleHover(projects[0].title)}
+            onClick={() => handleNavigate(`/${projects[0].title}`)}
+          >
             <p>G</p>
           </div>
 
-          <div className="o1" onMouseEnter={() => handleHover(projects[1].title)} onClick={() => handleNavigate(`/${projects[1].title}`)}>
+          <div
+            className="o1"
+            onMouseEnter={() => handleHover(projects[1].title)}
+            onClick={() => handleNavigate(`/${projects[1].title}`)}
+          >
             <p>O</p>
           </div>
 
-          <div className="g2" onMouseEnter={() => handleHover(projects[2].title)} onClick={() => handleNavigate(`/${projects[2].title}`)}>
+          <div
+            className="g2"
+            onMouseEnter={() => handleHover(projects[2].title)}
+            onClick={() => handleNavigate(`/${projects[2].title}`)}
+          >
             <p>G</p>
           </div>
 
-          <div className="o2" onMouseEnter={() => handleHover(projects[3].title)} onClick={() => handleNavigate(`/${projects[3].title}`)}>
+          <div
+            className="o2"
+            onMouseEnter={() => handleHover(projects[3].title)}
+            onClick={() => handleNavigate(`/${projects[3].title}`)}
+          >
             <p>O</p>
           </div>
 
-          <div className="s1" onMouseEnter={() => handleHover(projects[4].title)} onClick={() => handleNavigate(`/${projects[4].title}`)}>
+          <div
+            className="s1"
+            onMouseEnter={() => handleHover(projects[4].title)}
+            onClick={() => handleNavigate(`/${projects[4].title}`)}
+          >
             <p>S</p>
           </div>
 
-          <div className="s2" onMouseEnter={() => handleHover(projects[5].title)} onClick={() => handleNavigate(`/${projects[5].title}`)}>
+          <div
+            className="s2"
+            onMouseEnter={() => handleHover(projects[5].title)}
+            onClick={() => handleNavigate(`/${projects[5].title}`)}
+          >
             <p>S</p>
           </div>
 
-          <div className="e" onMouseEnter={() => handleHover(projects[6].title)} onClick={() => handleNavigate(`/${projects[6].title}`)}>
+          <div
+            className="e"
+            onMouseEnter={() => handleHover(projects[6].title)}
+            onClick={() => handleNavigate(`/${projects[6].title}`)}
+          >
             <p>E</p>
           </div>
         </div>
@@ -125,14 +179,7 @@ const Container = styled.div`
   padding-top: 20px;
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
   background-color: #dcdcdc;
-  padding-bottom: 100px;
-
-  .top {
-    display: flex;
-    justify-content: space-between;
-  }
 
   @media (max-width: 1100px) {
     justify-content: center;
@@ -144,7 +191,6 @@ const Container = styled.div`
   }
   @media (max-width: 1200px) {
     height: 750px;
-    
   }
 
   @media (max-width: 800px) {
@@ -153,27 +199,24 @@ const Container = styled.div`
   }
 `;
 
-const ImgBox = styled.div`
+const ImgBox = styled(motion.div)`
+  padding-top: 50px;
   div {
     display: flex;
     justify-content: space-between;
     margin-bottom: 20px;
     font-family: Authentic90;
-  }
-
-  .cadre {
     position: relative;
-    width: 38vw;
-    height: 18vw;
+    width: 600px;
+    height: 250px;
     overflow: hidden;
     border: none;
   }
-
   img {
     transition: 500ms;
     border: none;
-    width: 38vw;
-    height: 18vw;
+    width: 600px;
+    height: 250px;
     position: absolute;
     object-fit: cover;
     animation: ${appearAnimation} 800ms forwards;
@@ -184,13 +227,18 @@ const ImgBox = styled.div`
     }
   }
 
+  .project-text{
+    height: 20px;
+  }
+
   .fade-out {
     animation: ${clearAnimation} 500ms forwards;
   }
 
   @media (max-width: 1100px) {
     div {
-      max-width: 91vw;
+      
+      max-width: 71vw;
       margin-top: 20px;
 
       @media (max-width: 750px) {
@@ -198,7 +246,7 @@ const ImgBox = styled.div`
       }
     }
 
-    .cadre {
+    div {
       width: 100%;
       height: 40vw;
     }
@@ -211,14 +259,11 @@ const ImgBox = styled.div`
 
   @media (max-width: 500px) {
     div {
-      max-width: 90vw;
-      margin-top: 20px;
-    }
-
-    .cadre {
       width: 100%;
       height: 70vw;
     }
+
+    
 
     img {
       width: 100%;
